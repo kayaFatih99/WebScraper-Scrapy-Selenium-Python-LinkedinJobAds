bbed15267881813e124499d45ce0bee202c01a2d
kayaFatih99 <kayafatih051@gmail.com>
2022-11-14 15:43:11 +0300

    webScraper|Linkedin

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -1,7 +1,7 @@
 from pyexcel.cookbook import merge_all_to_a_book
 import glob
 
-csv_file = 'london_mobile_jobs'
-xlsx_file = 'london_mobile_jobs'
+csv_file = 'berlin_mobile_jobs'
+xlsx_file = 'berlin_mobile_jobs'
 
 merge_all_to_a_book(glob.glob(f"output/{csv_file}.csv"), f"output_excel/{xlsx_file}.xlsx")
